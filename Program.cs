b76228e3cf565fac0843ac687e7ec849abd55fbe
Greg Doud <gpdoud@gmail.com>
2018-10-05 07:43:54 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_world_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dotnet Core World!");
+        }
+    }
+}
